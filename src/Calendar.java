19f0870677b2dd292782bcad2804f53366dfebe1
haeky <haeky@DESKTOP-GQH4R2D>
2021-10-29 08:25:00 +0900

    Add repeat function

--- src/Calendar.java
+++ src/Calendar.java
@@ -7,24 +7,29 @@ public class Calendar {
 		return MAX_DAYS[month-1];
 	}
 	
-	public void printSampleCalendar() {
+	/*public void printSampleCalendar() {
 		System.out.println("일  월  화  수  목  금  토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-	}
+	}*/
 	
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in); 
 		Calendar cal = new Calendar();
+		System.out.println("반복횟수 입력");
+		int repeat = scanner.nextInt();
 		
-		System.out.println("달을 입력하세요");
-		int month = scanner.nextInt();
-		System.out.printf("%d월은 %d일까지 있습니다.\n",month,cal.getMaxDaysOfMonth(month));
-		cal.printSampleCalendar();
+		for (int i=0; i<repeat; i++) {
+			System.out.println("달을 입력하세요");
+			int month = scanner.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다.\n",month,cal.getMaxDaysOfMonth(month));
+			//cal.printSampleCalendar();
+		}
+		System.out.println("Bye");
 		scanner.close();
 	}
 
