397395dfbeb326a613f58b8c76723a2a68d3482e
haeky <haeky@DESKTOP-GQH4R2D>
2021-10-30 15:47:28 +0900

    Implement printing calendar (auto version)

--- src/Calendar.java
+++ src/Calendar.java
@@ -18,19 +18,20 @@ public class Calendar {
 		}
 	}
 	
-	public void printCalendar(int year, int month, int weekday) {
-		System.out.printf("    <<%4d년%3d월>>\n",year, month);
+	public void printCalendar(int year, int month) {
+		System.out.printf("    <<%d년%d월>>\n",year, month);
 		System.out.println(" SU MO TH WE TH FR SA");
 		System.out.println("---------------------");
 			
+		int weekday = getWeekDay(year, month, 1);
+		
 		for (int i=0; i<weekday; i++){
 			System.out.print("   ");
-			
 		}
 		
 		int maxDay = getMaxDaysOfMonth(year, month);
 		int count = 7 - weekday;
-		int delim = (count<7)? count : 0;
+		int delim = (count<7) ? count : 0;
 			
 		for(int i=1; i<=count; i++) {
 			System.out.printf("%3d",i);
@@ -45,6 +46,27 @@ public class Calendar {
 		}
 		System.out.println();
 		System.out.println();
+	}
+
+	private int getWeekDay(int year, int month, int day) {
+		int syear = 1970;
+		final int STANDARD_WEEKDAY=3; //1970/Jan/1st = Thursday
+		
+		int count = 0;
+		
+		for(int i=syear; i<year; i++) {
+			int delta = isLeapYear(i) ? 366 : 365;
+			count += delta;
+		}
+		
+		for (int i=1; i < month; i++) {
+			int delta = getMaxDaysOfMonth(year, i);
+			count += delta;
+		}
+		count += day;
+		
+		int weekday = (count+STANDARD_WEEKDAY) % 7;
+		return weekday;
 	}
 	
 }
