3135a1892e713acf5aebe79ffb3480118ff27e9d
haeky <haeky@DESKTOP-GQH4R2D>
2021-10-30 14:57:16 +0900

    Add update calendar

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Calendar {
 
 	private static final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
@@ -20,20 +18,33 @@ public class Calendar {
 		}
 	}
 	
-	public void printCalendar(int year, int month) {
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("    <<%4d년%3d월>>\n",year, month);
 		System.out.println(" SU MO TH WE TH FR SA");
 		System.out.println("---------------------");
 			
+		for (int i=0; i<weekday; i++){
+			System.out.print("   ");
+			
+		}
 		
 		int maxDay = getMaxDaysOfMonth(year, month);
-		
-		for(int i=1; i<=maxDay; i++) {
+		int count = 7 - weekday;
+		int delim = (count<7)? count : 0;
+			
+		for(int i=1; i<=count; i++) {
 			System.out.printf("%3d",i);
-			if(i % 7 == 0) {
+		}
+		System.out.println();
+		
+		for(int i=count+1; i<=maxDay; i++) {
+			System.out.printf("%3d",i);
+			if(i % 7 == delim) {
 				System.out.println();
 			}
 		}
 		System.out.println();
+		System.out.println();
 	}
+	
 }
--- src/Prompt.java
+++ src/Prompt.java
@@ -1,34 +1,56 @@
 import java.util.Scanner;
 
 public class Prompt {
-
-	private static final String PROMPT ="> ";
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in); 
+	
+	public int parseDay(String week) {
+		if (week.equals("su")) return 0;
+		else if(week.equals("mo")) return 1;
+		else if(week.equals("tu")) return 2;
+		else if(week.equals("we")) return 3;
+		else if(week.equals("th")) return 4;
+		else if(week.equals("fr")) return 5;
+		else if(week.equals("sa")) return 6;
+		else return 0;
+		
+	}
+		
+	public void runPrompt(){
+		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		
-		
 		int month = 1;
-		int year = -1;
+		int year = 2017;
+		int weekday=0;
+		
 		while (true) {
-			System.out.println("년도를 입력하세요");
+			System.out.println("년도를 입력하세요. (EXIT: -1)");
 			System.out.print("YEAR> ");
 			year = scanner.nextInt();
-			System.out.println("달을 입력하세요");
-			System.out.print("MONTH> ");
-			month = scanner.nextInt();
-			
-			if (month == -1) {
+			if(year == -1) {
 				break;
 			}
-			if(month>12) {
+			System.out.println("달을 입력하세요.");
+			System.out.print("MONTH> ");
+			month = scanner.nextInt();
+			if(month>12 || month<1) {
+				System.out.println("잘못된 입력입니다.");
 				continue;
 			}
+			System.out.println("첫번째 요일을 입력하세요. (su mo tu we th fr sa)");
+			System.out.print("WEEKDAY> ");
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
 			
-			cal.printCalendar(year, month);
+			cal.printCalendar(year, month, weekday);
 		}
 		System.out.println("Bye");
 		scanner.close();
+		
 	}
-
+	public static void main(String[] args) {
+		Prompt prompt = new Prompt();
+		prompt.runPrompt();
+		
+		
+	}
+	
 }
